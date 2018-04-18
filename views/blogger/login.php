dbfd90e853e2205a3bbef2d2d2aece64e7c685e8
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2018-04-18 20:18:38 +0100

    register button for the login page
    to link with Lovena's registration page

--- views/blogger/login.php
+++ views/blogger/login.php
@@ -8,11 +8,14 @@
             <form method="POST">
                 <label for="username" class="col-sm-2 col-form-label">Username</label>
                 <input type="text" name="username" class="form-control" id="Username" placeholder="username">
-             
+
                 <label for="password" class="col-sm-2 col-form-label">Password</label>
                 <input type="password" class="form-control" name="password"id="Password" placeholder="password">
                 <br/>
-                <center><button type="submit" name="submit"class="btn btn-primary">Sign in</button></center>
+                <center>
+                    <button type="submit" name="submit"class="btn btn-primary">Register</button>
+                    <button type="submit" name="submit"class="btn btn-primary">Sign in</button>
+                </center>
             </form>
         </div>
         <div class='col-md-4'>
