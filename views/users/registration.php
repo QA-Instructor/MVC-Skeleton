27e9df9a60af16f08b70d2b2406f1e1afd9d7520
Cynthujaa Satchi <cynthujaa.satchi@gmail.com>
2019-04-18 20:26:37 +0100

    Updated PHP code

--- views/users/registration.php
+++ views/users/registration.php
@@ -4,15 +4,23 @@
     <h2>Register</h2>
 </div>
     <p>
-        <input class="w3-input" type="text" name="userName" pattern=^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$ title="Your username must be at least 3 characters, begin with a letter, and may not contain special characters" required autofocus>
-        <label>User Name</label>
+        <input class="w3-input" type="text" name="firstName" pattern=^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$ title="First Name" required autofocus>
+        <label>First Name</label>
     </p>
-        <p>
+    <p>
+        <input class="w3-input" type="text" name="lastName" pattern=^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$ title="Last Name" required>
+        <label>Last Name</label>
+    </p>
+    <p>
+        <input class="w3-input" type="text" name="userName" pattern=^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$ title="Your username must be at least 3 characters, begin with a letter, and may not contain special characters" required>
+        <label>Username</label>
+    </p>
+    <p>
         <input class="w3-input" type="password" name="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$" title="Password must be at least 4 characters and must include at least one upper case letter, one lower case letter, and one number." required>
         <label>Password</label>
     </p>
     
-            <p>
+    <p>
         <input class="w3-input" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
         <label>E-mail</label>
     </p>
@@ -22,12 +30,12 @@
         <label>Phone Number</label>
     </p>
     
-            <p> Would you like to:
+            <p> Would you like to be a:
         <input class="w3-input" type="radio" name="userType" value="blogger">
-        <label>Blog</label><br>
+        <label>Blogger</label><br>
         Or
         <input class="w3-input" type="radio" name="userType" value="subscriber" checked>
-        <label>Subscribe</label>
+        <label>Subscriber</label>
     </p>
             
   <input type="hidden" 
