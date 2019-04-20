608309959211926be25a908b887dbe91f34f52b1
Dominika <do_ma1991@hotmail.com>
2019-04-20 12:48:17 +0100

    styling on nav-bar and footer

--- views/layout.php
+++ views/layout.php
@@ -11,19 +11,57 @@
         <title>Events Blog</title>
     </head>
     <body>
-        <header class="w3-container w3-gray">
+<!--        <header>
+            <div class="container">
+                <div class="row text-right">
+                    <div class="col-sm">
+                        
+                    </div>
             <a href='/MVC-Skeleton'>Home</a>
-            <!--These should only be available if no one is logged in-->
+            These should only be available if no one is logged in
             <a href="">Login</a>
             <a href="">Register</a>
-            <!--These should only be available after logging in-->
-            <!--      <a href="">My Profile</a>
+            These should only be available after logging in
+                  <a href="">My Profile</a>
                   <a href="">My Posts</a>
-                  <a href="">Logout</a>-->
+                  <a href="">Logout</a>
 
-            <!--      <a href='?controller=post&action=readAll'>Posts</a>
-                  <a href='?controller=post&action=create'>Add Post</a>-->
-        </header>
+                  <a href='?controller=post&action=readAll'>Posts</a>
+                  <a href='?controller=post&action=create'>Add Post</a>
+                </div>
+                </div>
+        </header>-->
+
+<nav class="navbar navbar-expand-lg">
+    <div class="d-flex flex-grow-1">
+        <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+        <a class="navbar-brand d-none d-lg-inline-block" href="#">
+            Name and Logo
+        </a>
+        <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
+            Name and <img src="" alt="logo">
+        </a>
+        <div class="w-100 text-right">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+        <ul class="navbar-nav ml-auto flex-nowrap">
+            <li class="nav-item">
+                <a href="#" class="nav-link m-2 menu-item nav-active">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link m-2 menu-item">Login</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link m-2 menu-item">Register</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
         <div class="w3-container">
             <?php require_once('routes.php'); ?>
             </<div>
@@ -81,5 +119,10 @@
                         Copyright &COPY; Women In Tech <?= date('Y'); ?> . All rights reserved.
                     </footer>
                 </div>
+                                
+                                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                                
                 </body>
                 </html>
