e615fd12329bc06a3e16204bf77309c8c6e23a69
Claudia Gardner-Pickett <claudiagardner-pickett@Claudias-Mac.fritz.box>
2020-05-24 11:20:16 +0100

    Error page styling

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,6 +1,21 @@
-<div class="w3-container w3-red">
-<p> Page under construction. </p>
-<img src="../MVC-Skeleton/views/pages/errorrelax.jpg" width="150" />
-<p>Please relax with Suki whilst we work on it.</p>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/flatly/bootstrap.min.css">
+
+<div class=" container fluid my-4">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm">
+                <h1 class="display-3"> Oops! We're a little lost. </h1>
+                <div>
+                    <p class="lead">Relax with Suki whilst we get back on track <br><br></p>
+                </div> 
+                <p class="display-5">Hike back to the <a href='/MVC-Skeleton'>Home</a> Page?   </p>
+            </div>
+            <div class="col-sm">
+                <img src="errorrelax.jpg" class="responsive rounded" style="display:inline" width="100%"/>
+            </div>
+        </div>
+    </div>
+
 </div>
 
