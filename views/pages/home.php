47fa6cb779522157aacadddc734bf0ff32ae342b
Bushra Alakraa <alakraa.bushra@gmail.com>
2020-05-25 12:25:18 +0100

    views pages

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,4 +1,110 @@
-<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
+<!DOCTYPE html>
+<html>
+ <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title>Our Blog</title>
+    <!--We need the meta tag to properly scale the responsive CSS code-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Here I went ahead and imported two fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900|Cormorant+Garamond:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="views/css/style.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
 
-    
-<!--    // Bushra's landing page -->
+    <main>
+      <!--The website banner START-->
+      <section class="index-banner">
+        <div class="vertical-center">
+          <h2>Welcome to our Travel Blog<br>Explore the world with us</h2>
+          <h1>Our blog is not just about photography, it will take you on a journey of culture, food, history and beyond</h1>
+           <div class="container">
+<div class="input-group">
+	<input type="text" class="form-control" placeholder="Search for...">
+      <span class="input-group-btn">
+        <button class="btn btn-search" type="button"><i class="fa fa-search fa-fw"></i> Search</button>
+      </span>
+</div>
+</div>
+        </div>
+                 
+         
+      
+     
+      <!--The website banner END-->
+      </section>
+
+       
+         
+          
+      <!--First wrapper START-->
+      <div class="wrapper">
+        <!--The links/boxes bellow the banner START-->
+        <section class="index-links">
+          <a href="cases.html">
+            <div class="index-boxlink-square">
+              <h3>Morocco</h3>
+            </div>
+          </a>
+          <a href="#">
+            <div class="index-boxlink-rectangle">
+              <h3>Tanzania</h3>
+            </div>
+          </a>
+            <a href="#">
+            <div class="index-boxlink-square">
+              <h3>Turkey</h3>
+            </div>
+          </a>
+            <a href="#">
+            <div class="index-boxlink-rectangle">
+              <h3>Poland</h3>
+            </div>
+          </a>
+            <a href="#">
+            <div class="index-boxlink-square">
+              <h3>Vietnam</h3>
+            </div>
+          
+        <!--The links/boxes bellow the banner END-->
+        </section>
+      <!--First wrapper END-->
+      </div>
+    <!--The main content END-->
+    </main>
+
+    <!--Second wrapper START-->
+    <div class="wrapper">
+      <!--The footer START-->
+      <footer>
+        <!--The main footer menu-->
+        <ul class="footer-links-main">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">Subscribe and Login</a></li>
+        </ul>
+        <!--The latest cases footer menu-->
+     
+        <!--The social media footer menu-->
+        <div class="footer-sm">
+          <a href="#">
+            <img src="views/images/img/youtube-symbol.png" alt="youtube icon">
+          </a>
+          <a href="#">
+            <img src="views/images/img/twitter-logo-button.png" alt="youtube icon">
+          </a>
+          <a href="#">
+            <img src="views/images/img/facebook-logo-button.png" alt="youtube icon">
+          </a>
+        </div>
+      <!--The footer END-->
+      </footer>
+    <!--Second wrapper END-->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
