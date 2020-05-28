4e24d28d899055eb638e705f83ce346cb9b00712
Fatima-93 <fatimaalahmad@googlemail.com>
2020-05-28 19:28:14 +0100

    google translate API integrated to readAll.php - works!

--- views/blogpost/readAll.php
+++ views/blogpost/readAll.php
@@ -29,7 +29,17 @@
 
       <!-- ForEach over our blogposts -->
         
-         
+   <p>Translate this page:</p>
+
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>      
   
 
 <!-- Banner -->
