7ce6b868b9c7158536fc41f35ef1a019d9a3c26c
Fatima-93 <fatimaalahmad@googlemail.com>
2020-05-29 19:25:03 +0100

    search nav link on nav bar fixed - no longer makes the navbar go crazy

--- views/user/ajax.php
+++ views/user/ajax.php
@@ -11,7 +11,7 @@ and open the template in the editor.
         <title>Blog Posts search via PHP, AJAX and jQuery</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+<!--        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />-->
     </head>
     <body>
         <div class="container">
@@ -19,7 +19,7 @@ and open the template in the editor.
             <h2 align="center"></h2><br />
             <div class="form-group">
                 <div class="input-group">
-                    <span class="input-group-addon">Search</span>
+                    <span class="input-group-addon"></span>
                     <input type="text" name="search_text" id="search_text" placeholder="Search for your blog post by name, title or content" class="form-control" />
                 </div>
             </div>
