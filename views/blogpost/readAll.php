3b9a537a4f854340f61cc8d4fd100cad1a709797
F-lo <pascaruflorentina2@gmail.com>
2020-05-30 16:53:32 +0100

    styling for allblogs page and wow page

--- views/blogpost/readAll.php
+++ views/blogpost/readAll.php
@@ -1,34 +1,35 @@
  
-    
-                <div id="realTimeContents">         
+
+<!--<div id="realTimeContents">         
 
 <div class="container">
-    <div class="row">
-        <div class="col">
-            <!--      1 of 3-->
-        </div>
-        <div class="col-6">
-            <!--      2 of 3 (wider)-->
-        </div>
-        <div class="col">
-            <!--      3 of 3-->
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <!--      1 of 3-->
-        </div>
-        <div class="col-5">
-            <!--      2 of 3 (wider)-->
-        </div>
-        <div class="col">
-            <!--      3 of 3-->
-        </div>
-    </div>
+   <div class="row">
+       <div class="col">
+<!--      1 of 3
 </div>
+<div class="col-6">
+<!--      2 of 3 (wider)
+</div>
+<div class="col">
+3 of 3
+</div>
+</div>
+<div class="row">
+<div class="col">z
+1 of 3
+</div>
+<div class="col-5">
+2 of 3 (wider)
+</div>
+<div class="col">
+3 of 3
+</div>
+</div>
+</div>
+</div> -->
 
-      <!-- ForEach over our blogposts -->
-        
+<!-- ForEach over our blogposts -->
+
 <!--   //Attempts to move the translate API to the right hand side:-->
 <!--<div dir="rtl" lang="">-->
 <!--<style>
@@ -43,78 +44,98 @@
     align: center;
 }
 </style>-->
-           <p></p>
+<p></p>
 
 <div class="container-fluid">
     <div class="row">
-    <div class="col-lg-10"></div>
+        <div class="col-lg-10"></div>
         <div class="col-lg-2" align="right">
-<div id="google_translate_element"> 
-<script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-}
-</script>
+            <div id="google_translate_element"> 
+                <script type="text/javascript">
+                    function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                    }
+                </script>
 
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>      
-</div>
-</div>
+                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>      
+            </div>
+        </div>
     </div>
-    </div>
+</div>
 <!-- Banner -->
 
-<div class="jumbotron">
-    <h1 class="display-3">Welcome to Pawsome</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p>
-</div>
-
-<!-- Page Content -->
-<div class="container">
-    <div class="row">
-
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-    
-            <!-- Blog Post #1 -->
-            <div class="card mb-4">
-                
-                 <?php foreach ($blogposts as $blogpost) {                 
-     try{    
-         if(empty($blogpost->blogPostPhoto)){
-             throw new Exception("Picture is not available");
-         }else{    
-             $file = $blogpost->blogPostPhoto;
-
-    $file = explode('/', $file, 5);      
-   echo "<div>";
-    $img = "<img class='card-img-top' src='$file[4]' alt='Card image cap' style='height: 700px; width: 100%; display: block;' />";
-
-         echo $img;      }
-     } catch (Exception $e){
-         echo 'Message: ' .$e->getMessage();
-     }
-?>                
-<!--                <img class="card-img-top" src='' alt="Card image cap">-->
-                <div class="card-body">
-                    <h2 class="card-title">                    
-                                <?php echo $blogpost->blogPostName; ?> 
-                    </h2>
-                    <p class="card-text">   <?php echo $blogpost->blogPostSubName; ?>  </p>
-                     <a class="btn btn-primary" href='?controller=blogpost&action=read&id=<?php echo $blogpost->blogpostID; ?>'>Read More &rarr;</a>    
-                </div>
-                <div class="card-footer text-muted">
-                    <?php echo $blogpost->datePosted; ?> 
-<!--                    Posted on January 1, 2017 -->
-                    <a href='?controller=blogpost&action=update&id=<?php echo $blogpost->blogpostID; ?>'>Edit</a>
-                    <a href='?controller=blogpost&action=delete&id=<?php echo $blogpost->blogpostID; ?>'>Delete</a>
-                </div>
+<div class="container-fluid">
+    <div class =" container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <h1 class="display-3 py-3">Welcome to Pawsome</h1>
+                <p class="lead">A place for amazing stories about the cutest, funniest and most adventurous furry beings.</p>
+                <!--<hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>-->
             </div>
-            <?php } ?>
+            <div class="col-md-3"></div> 
+        </div>
+    </div>
+
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <!-- Blog Entries Column -->
+
+                <!-- Blog Post #1 -->
+                <div class="card mb-4">
+                    <?php
+                    foreach ($blogposts as $blogpost) {
+                        try {
+                            if (empty($blogpost->blogPostPhoto)) {
+                                throw new Exception("Picture is not available");
+                            } else {
+                                $file = $blogpost->blogPostPhoto;
+
+                                $file = explode('/', $file, 5);
+                                //echo "<div>";
+                                $img = "<img class='card-img-top' src='$file[4]' alt='Card image cap';/>";
+
+                                echo $img;
+                            }
+                        } catch (Exception $e) {
+                            echo 'Message: ' . $e->getMessage();
+                        }
+                        ?>  
+
+
+        <!--                <img class="card-img-top" src='' alt="Card image cap">-->
+
+                        <div class="card-body">
+                            <h2 class="card-title">                    
+                                <?php echo $blogpost->blogPostName; ?> 
+                            </h2>
+                            <p class="card-text">   <?php echo $blogpost->blogPostSubName; ?>  </p>
+                            <a class="btn btn-primary" href='?controller=blogpost&action=read&id=<?php echo $blogpost->blogpostID; ?>'>Read More &rarr;</a>    
+                        </div>
+                        <div class="card-footer text-muted">
+                            <?php echo $blogpost->datePosted; ?> 
+                            <!--                    Posted on January 1, 2017 -->
+                            <a href='?controller=blogpost&action=update&id=<?php echo $blogpost->blogpostID; ?>'>Edit</a>
+                            <a href='?controller=blogpost&action=delete&id=<?php echo $blogpost->blogpostID; ?>'>Delete</a> 
+                        </div>
+                    <?php } ?> 
+                    
+                </div> 
+                <div><br></div>
+            </div> 
+            <div class="col-md-2"></div>
+        </div>
+
+    </div>
+</div>
 
 <!--             Blog Post #2 
             <div class="card mb-4">
@@ -130,20 +151,19 @@ function googleTranslateElementInit() {
                 </div>
             </div>-->
 
-            <!--           Side Widget 
-                    <div class="card my-4">
-                      <h5 class="card-header">Side Widget</h5>
-                      <div class="card-body">
-                        You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-                      </div>
-                    </div>
-            
-                  </div>
-            
-                </div>
-                 /.row 
-            
-              </div>
-               /.container -->
+<!--           Side Widget 
+        <div class="card my-4">
+          <h5 class="card-header">Side Widget</h5>
+          <div class="card-body">
+            You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+          </div>
+        </div>
 
-         
+      </div>
+
+    </div>
+     /.row 
+
+  </div>
+   /.container -->
+
